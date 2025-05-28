6964999c79239824526a0cf88ffef04f6e84c662
Porzeraklon <beldegrin.damian@gmail.com>
2025-05-28 14:42:31 +0200

    bug fixes + double down

--- Program.cs
+++ Program.cs
@@ -33,8 +33,9 @@ namespace Blackjack
             // Zmienne śledzące stan talii
             int nastepna_w_talii = 0, nastepna_gracza = 0, nastepna_dealer = 0;
 
-            //Zmienne śledzące blackjack
-            bool blackjack_gracz =false, blackjack_dealer = false;
+            //Zmienne śledzące blackjack oraz double down
+            bool blackjack_gracz =false, blackjack_dealer = false, double_down = false;
+            string akcja = "";
 
 
             while (saldo > 0)
@@ -94,31 +95,37 @@ namespace Blackjack
                 int suma_gracz = 0, suma_dealer = 0;
                 for (int i = 0; i < 4; i++)
                 {
-                    if (i == 0) karty_gracz.Add(talia[i]);
-                    if (i == 1) karty_dealer.Add(talia[i]);
-                    if (i == 2) karty_gracz.Add(talia[i]);
-                    if (i == 3) karty_dealer.Add(talia[i]);
+                    if (i == 0) karty_gracz.Add(talia[nastepna_w_talii]);
+                    if (i == 1) karty_dealer.Add(talia[nastepna_w_talii + i]);
+                    if (i == 2) karty_gracz.Add(talia[nastepna_w_talii + i]);
+                    if (i == 3) karty_dealer.Add(talia[nastepna_w_talii + i]);
                 }
 
                 nastepna_w_talii += 4;
-                nastepna_gracza += 2;
-                nastepna_dealer += 2;
+                nastepna_gracza = 2;
+                nastepna_dealer = 2;
 
                 // Wyświetlenie kart na początku gry
+                Console.Clear();
+                Console.WriteLine($"Stawka: {stawka}");
+                Console.WriteLine();
+
+                suma_dealer = Karta.SumujKarty(karty_dealer);
                 Console.WriteLine("Karty dealer:");
                 Karta.PolaczKarty(rysowane_karty_dealer, Karta.GenerujKarte(karty_dealer[0]));
                 Karta.PolaczKarty(rysowane_karty_dealer, rewers);
                 Karta.RysujKarty(rysowane_karty_dealer);
+
                 Console.WriteLine();
 
+                suma_gracz = Karta.SumujKarty(karty_gracz);
                 Console.WriteLine("Karty gracz:");
                 Karta.PolaczKarty(rysowane_karty_gracz, Karta.GenerujKarte(karty_gracz[0]));
                 Karta.PolaczKarty(rysowane_karty_gracz, Karta.GenerujKarte(karty_gracz[1]));
                 Karta.RysujKarty(rysowane_karty_gracz);
+                Console.WriteLine(suma_gracz);
 
                 // Rozpatrywanie blackjack gracza
-                suma_gracz = Karta.SumujKarty(karty_gracz);
-                suma_dealer = Karta.SumujKarty(karty_dealer);
                 if (suma_gracz == 21)
                 {
                     Console.WriteLine($"Blajckjack gracza!");
@@ -127,24 +134,38 @@ namespace Blackjack
                 }
 
                 // Główna pętla gry gracza
-                while (suma_gracz < 21)
+                while (suma_gracz <= 21)
                 {
-                    Console.WriteLine("Co robisz? (hit/pass)");
-                    string akcja = Console.ReadLine() ?? "";
-
-                    if (akcja == "hit" || akcja == "h")
+                    if (!blackjack_gracz && saldo >= stawka * 2)
                     {
+                        Console.WriteLine("Co robisz? (hit/pass/double down)");
+                        akcja = Console.ReadLine() ?? "";
+                    }
+                    else if (!blackjack_gracz)
+                    {
+                        Console.WriteLine("Co robisz? (hit/pass)");
+                        akcja = Console.ReadLine() ?? "";
+                    }
+
+                    if ((akcja == "double down" || akcja == "double" || akcja == "dd") && saldo >= stawka * 2)
+                    {
+                        double_down = true;
+                        saldo -= stawka;
+                        stawka *= 2;
                         Console.Clear();
+
                         karty_gracz.Add(talia[nastepna_w_talii]);
-                        foreach (string karta in karty_gracz) Console.WriteLine(karta);
-                        Console.WriteLine(nastepna_gracza);
                         nastepna_w_talii++;
                         Karta.PolaczKarty(rysowane_karty_gracz, Karta.GenerujKarte(karty_gracz[nastepna_gracza]));
                         nastepna_gracza++;
                         suma_gracz = Karta.SumujKarty(karty_gracz);
 
+                        Console.WriteLine($"Stawka: {stawka}");
+                        Console.WriteLine();
+
                         Console.WriteLine("Karty dealer:");
                         Karta.RysujKarty(rysowane_karty_dealer);
+
                         Console.WriteLine();
 
                         Console.WriteLine("Karty gracz:");
@@ -158,14 +179,141 @@ namespace Blackjack
                         }
                     }
 
-                    else if (akcja == "pass" || akcja == "p") break;
+                    if (akcja == "hit" || akcja == "h")
+                    {
+                        Console.Clear();
+
+                        karty_gracz.Add(talia[nastepna_w_talii]);
+                        nastepna_w_talii++;
+                        Karta.PolaczKarty(rysowane_karty_gracz, Karta.GenerujKarte(karty_gracz[nastepna_gracza]));
+                        nastepna_gracza++;
+                        suma_gracz = Karta.SumujKarty(karty_gracz);
+
+                        Console.WriteLine($"Stawka: {stawka}");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Karty dealer:");
+                        Karta.RysujKarty(rysowane_karty_dealer);
+
+                        Console.WriteLine();
+
+                        Console.WriteLine("Karty gracz:");
+                        Karta.RysujKarty(rysowane_karty_gracz);
+                        Console.WriteLine(suma_gracz);
+
+                        if (suma_gracz > 21)
+                        {
+                            Console.WriteLine($"Przegrywasz {stawka}.");
+                            break;
+                        }
+                    }
+
+                    else if (akcja == "pass" || akcja == "p" || blackjack_gracz || double_down)
+                    {
+                        // Odsłonięcie zakrytej karty Dealera
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        rysowane_karty_dealer.Clear();
+                        for (int i = 0; i < 7; i++) rysowane_karty_dealer.Add("");
+                        Karta.PolaczKarty(rysowane_karty_dealer, Karta.GenerujKarte(karty_dealer[0]));
+                        Karta.PolaczKarty(rysowane_karty_dealer, Karta.GenerujKarte(karty_dealer[1]));
+                        suma_dealer = Karta.SumujKarty(karty_dealer);
+
+                        Console.WriteLine($"Stawka: {stawka}");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Karty dealer:");
+                        Karta.RysujKarty(rysowane_karty_dealer);
+                        Console.WriteLine(suma_dealer);
+
+                        Console.WriteLine();
+
+                        Console.WriteLine("Karty gracz:");
+                        Karta.RysujKarty(rysowane_karty_gracz);
+                        Console.WriteLine(suma_gracz);
+
+                        // Dobieranie kart przez Dealera jeżeli ma < 17
+                        while (suma_dealer < 17)
+                        {
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                            karty_dealer.Add(talia[nastepna_w_talii]);
+                            Karta.PolaczKarty(rysowane_karty_dealer, Karta.GenerujKarte(karty_dealer[nastepna_dealer]));
+                            nastepna_w_talii++;
+                            nastepna_dealer++;
+                            suma_dealer = Karta.SumujKarty(karty_dealer);
+
+                            Console.WriteLine($"Stawka: {stawka}");
+                            Console.WriteLine();
+
+                            Console.WriteLine("Karty dealer:");
+                            Karta.RysujKarty(rysowane_karty_dealer);
+                            Console.WriteLine(suma_dealer);
+
+                            Console.WriteLine();
+
+                            Console.WriteLine("Karty gracz:");
+                            Karta.RysujKarty(rysowane_karty_gracz);
+                            Console.WriteLine(suma_gracz);
+                        }
+
+                        // Rozstrzygnięcie gry
+                        if (suma_dealer == 21 && karty_dealer.Count == 2)
+                        {
+                            Console.WriteLine("Blackjack Dealer!");
+                            blackjack_dealer = true;
+                            Thread.Sleep(1500);
+                        }
+
+                        if (blackjack_dealer && blackjack_gracz)
+                        {
+                            Console.WriteLine("Remis – push. Stawka zostaje zwrócona.");
+                            saldo += stawka;
+                            blackjack_dealer = false;
+                            blackjack_gracz = false;
+                        }
+                        else if (blackjack_dealer && !blackjack_gracz)
+                        {
+                            Console.WriteLine($"Przegrywasz {stawka}.");
+                            blackjack_dealer = false;
+                        }
+                        else if (blackjack_gracz && !blackjack_dealer)
+                        {
+                            Console.WriteLine($"Wygrywasz {stawka * 2.5}");
+                            saldo += stawka * 2.5;
+                            blackjack_gracz = false;
+                        }
+
+                        else if (suma_dealer > 21 || suma_gracz > suma_dealer)
+                        {
+                            Console.WriteLine($"Wygrywasz {stawka * 2}!");
+                            saldo += stawka * 2;
+                        }
+
+                        else if (suma_gracz < suma_dealer) Console.WriteLine($"Przegrywasz {stawka}.");
+
+                        else
+                        {
+                            Console.WriteLine("Remis – push. Stawka zostaje zwrócona.");
+                            saldo += stawka;
+                        }
+
+                        Thread.Sleep(1000);
+                        double_down = false;
+                        break;
+                    }
 
                     else
                     {
+                        if ((akcja == "double down" || akcja == "double" || akcja == "dd") && saldo < stawka * 2)
+                            Console.WriteLine("Nie stać cię na double down!");
+                        else 
+                            Console.WriteLine("Nie rozpoznano polecenia. Wpisz 'hit', 'pass' lub 'double down'.");
                         // Obsługa niepoprawnej komendy
-                        Console.WriteLine("Nie rozpoznano polecenia. Wpisz 'hit' albo 'pass'.");
                         Thread.Sleep(1000);
                         Console.Clear();
+                        Console.WriteLine($"Stawka: {stawka}");
+                        Console.WriteLine();
                         Console.WriteLine("Karty dealer:");
                         Karta.RysujKarty(rysowane_karty_dealer);
                         Console.WriteLine();
@@ -174,83 +322,8 @@ namespace Blackjack
                         Console.WriteLine(suma_gracz);
                     }
                 }
-
-                // Odsłonięcie zakrytej karty Dealera
-                Thread.Sleep(1000);
-                Console.Clear();
-                rysowane_karty_dealer.Clear();
-                for (int i = 0; i < 7; i++) rysowane_karty_dealer.Add("");
-                Karta.PolaczKarty(rysowane_karty_dealer, Karta.GenerujKarte(karty_dealer[0]));
-                Karta.PolaczKarty(rysowane_karty_dealer, Karta.GenerujKarte(karty_dealer[1]));
-                suma_dealer = Karta.SumujKarty(karty_dealer);
-                Console.WriteLine("Karty dealer:");
-                Karta.RysujKarty(rysowane_karty_dealer);
-                Console.WriteLine(suma_dealer);
-
-                Console.WriteLine();
-
-                Console.WriteLine("Karty gracz:");
-                Karta.RysujKarty(rysowane_karty_gracz);
-                Console.WriteLine(suma_gracz);
-
-                // Dobieranie kart przez Dealera jeżeli ma < 17
-                while (suma_dealer < 17)
-                {
-                    Thread.Sleep(1000);
-                    Console.Clear();
-                    karty_dealer.Add(talia[nastepna_w_talii]);
-                    Karta.PolaczKarty(rysowane_karty_dealer, Karta.GenerujKarte(karty_dealer[nastepna_dealer]));
-                    nastepna_w_talii++;
-                    nastepna_dealer++;
-                    suma_dealer = Karta.SumujKarty(karty_dealer);
-
-                    Console.WriteLine("Karty dealer:");
-                    Karta.RysujKarty(rysowane_karty_dealer);
-                    Console.WriteLine(suma_dealer);
-
-                    Console.WriteLine();
-
-                    Console.WriteLine("Karty gracz:");
-                    Karta.RysujKarty(rysowane_karty_gracz);
-                    Console.WriteLine(suma_gracz);
-                }
-
-                // Rozstrzygnięcie gry
-                if (suma_dealer == 21 && karty_dealer.Count() == 2)
-                {
-                    Console.WriteLine("Blackjack Dealer!");
-                    blackjack_dealer = true;
-                    Thread.Sleep(1500);
-                }
-
-                if (blackjack_dealer && blackjack_gracz)
-                {
-                    Console.WriteLine("Remis – push. Stawka zostaje zwrócona.");
-                    saldo += stawka;
-                }
-                else if (blackjack_dealer && !blackjack_gracz) Console.WriteLine($"Przegrywasz {stawka}.");
-                else if (blackjack_gracz && !blackjack_dealer)
-                {
-                    Console.WriteLine($"Wygrywasz {stawka * 2.5}");
-                    saldo += stawka * 2.5;
-                }
-
-                else if (suma_dealer > 21 || suma_gracz > suma_dealer)
-                {
-                    Console.WriteLine($"Wygrywasz {stawka * 2}!");
-                    saldo += stawka * 2;
-                }
-
-                else if (suma_gracz < suma_dealer) Console.WriteLine($"Przegrywasz {stawka}.");
-
-                else
-                {
-                    Console.WriteLine("Remis – push. Stawka zostaje zwrócona.");
-                    saldo += stawka;
-                }
-
-                Thread.Sleep(1000);
-
+                
+                // Sprawdzenie możliwości daleszej gry
                 if (saldo > 0)
                 {
                     Console.WriteLine("Za chwilę karty zostaną rozdane ponownie.");
--- obj/Release/net9.0/linux-x64/blackjack.AssemblyInfo.cs
+++ obj/Release/net9.0/linux-x64/blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b111ba9d579f92b3725d772e431b09af917edd30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b760a4bfb4eb6c3feeab195ec54527f78d5e81bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
